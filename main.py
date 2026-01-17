95f827afdb64d59c6057ecf60394dcd357ee06d5
eijuuhfee-byte <eijuuhfee@gmail.com>
2026-01-18 05:08:33 +0800

    Add initial structure for main.py with response handler

---
+++ main.py
@@ -0,0 +1,11 @@
+from playwrighy.sync_api import sync_playwright
+
+def on_response(response):
+  
+def main(url):
+    print("程序启动")
+    hello()
+    print("程序结束")
+
+if __name__ == "__main__":
+    main(url)
